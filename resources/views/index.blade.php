d4904e9a241b91e3622180a3348710f910c4c1af
ShiruRino <tigugg654@gmail.com>
2025-08-19 10:42:01 +0700

    Added user seeder

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,10 +22,11 @@
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
                 <form action="../index3.html" method="post">
-                    <div class="input-group mb-3"> <input type="email" class="form-control" placeholder="Email">
+                    @csrf
+                    <div class="input-group mb-3"> <input type="email" class="form-control" name="email" placeholder="Email">
                         <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
                     </div>
-                    <div class="input-group mb-3"> <input type="password" class="form-control" placeholder="Password">
+                    <div class="input-group mb-3"> <input type="password" class="form-control" name='password' placeholder="Password">
                         <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                     </div> <!--begin::Row-->
                     <div class="row">
